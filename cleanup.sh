95dbf10b3a60eef1eeabb16c2a6bb9cd021c8dcd
Darren Smith <git@darrenjs.net>
2015-06-07 12:29:22 +0100

    updated license file

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,9 @@ find . -name Makefile.in -exec rm -f '{}' \;
 
 [ -d m4 ] && rm -rf m4
 
-for f in depcomp missing INSTALL install-sh config.sub config.guess compile config.h.in configure aclocal.m4 configure aclocal.m4 ltmain.sh ar-lib autom4te.cache
+for f in depcomp missing INSTALL install-sh config.sub config.guess compile \
+    config.h.in configure aclocal.m4 configure aclocal.m4 ltmain.sh ar-lib \
+    autom4te.cache COPYING
 do
     [ -d "$f" ] && rm -rf "$f"
     rm -f "$f"
